b981ebe83874b2b8c179939761c48b565151e086
tamarapaocic <tamara.paocic@hotmail.com>
2017-02-11 14:07:30 +0100

    ispravka

--- src/zadaci_09_02_2017/Zadatak4.java
+++ src/zadaci_09_02_2017/Zadatak4.java
@@ -1,17 +1,35 @@
 package zadaci_09_02_2017;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Zadatak4 {
 
 	public static void main(String[] args) {
 	
-		//Korisnikov unos
+		//novi scanner
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Unesite n: ");
-		int n = sc.nextInt();
+		int n = 0;
+		while(true) { //Prolazimo kroz petlju dok korisnik ne unese odgovarajucu vrijednost.
+		    try {
+				 n = sc.nextInt(); //korisnikov unos
+					printMatrix(n); //poziv metode za printanje
+				break;
+
+			}
+			catch(NegativeArraySizeException ex) { //Ukoliko korisnik unese negativan broj program ce ga pitat da pokusa ponovo
+				System.out.println("Pokusajte ponovo: ");
+				sc.nextLine();
+			}
+		    catch(InputMismatchException ex){ //Ukoliko korisnik ne unese broj program ce ga pitat da pokusa ponovo
+		    	System.out.println("Pokusajte ponovo: ");
+		    	sc.nextLine();
+		    }
+		    
+		}
+		
 		//Poziv metode za printanje n x n matrice
-		printMatrix(n);
 
 	}
 
