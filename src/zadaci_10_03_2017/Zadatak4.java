827f3bed417ab91915e3e2b953d34116afa2d134
tamarapaocic <tamara.paocic@hotmail.com>
2017-03-10 15:41:14 +0100

    ispravka

--- src/zadaci_10_03_2017/Zadatak4.java
+++ src/zadaci_10_03_2017/Zadatak4.java
@@ -1,12 +1,9 @@
 package zadaci_10_03_2017;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Scanner;
 
 public class Zadatak4 {
 
